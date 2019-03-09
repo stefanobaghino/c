9581b72675ce92b2958d4890eec34522ca2914bd
Stefano Baghino <stefano@baghino.me>
2019-03-09 13:55:28 +0100

    p013: for

--- ftoc.c
+++ ftoc.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	float fahr, celsius;
-	int lower, upper, step;
+	int fahr;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-	printf("  F      C\n");
-	while (fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr-32) ;
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 	}
 }
 
